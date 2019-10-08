f124adec9c19cb7d0b2f1a025c9383dc3c25b580
Stephen Chua <e0203150@u.nus.edu>
2019-10-08 20:39:16 +0800

    Enhancing BloodSugarCommand MESSAGE_USAGE

--- src/main/java/seedu/address/logic/commands/BloodSugarCommand.java
+++ src/main/java/seedu/address/logic/commands/BloodSugarCommand.java
@@ -10,11 +10,13 @@ public class BloodSugarCommand extends Command {
 
     public static final String COMMAND_WORD = "bloodsugar";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds the bloodsugar level at the current datetime"
-        + "Parameters: VALUE (must be a positive integer) "
-        + "v/ [BLOODSUGARLEVEL]\n"
-        + "Example: " + COMMAND_WORD
-        + "v/ 78.9.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds the bloodsugar level"
+        + "Existing bloodsugar will be overwritten by the input.\n"
+        + "Parameters: INDEX (must be a positive integer) "
+        + "b/ [BLOODSUGAR]\n"
+        + "Example: " + COMMAND_WORD + " 1 "
+        + "b/ 89.0.";
+
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Bloodsugar command not implemented yet";
 
