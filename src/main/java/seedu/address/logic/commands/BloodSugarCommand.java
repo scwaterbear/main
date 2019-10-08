26cf01467801f8a0a02812ec762afb86e86526ad
Stephen Chua <e0203150@u.nus.edu>
2019-10-08 19:59:01 +0800

    Change BloodSugarCommand to throw an exception

--- src/main/java/seedu/address/logic/commands/BloodSugarCommand.java
+++ src/main/java/seedu/address/logic/commands/BloodSugarCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +10,16 @@ public class BloodSugarCommand extends Command {
 
     public static final String COMMAND_WORD = "bloodsugar";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds the bloodsugar level at the current datetime"
+        + "Parameters: VALUE (must be a positive integer) "
+        + "v/ [BLOODSUGARLEVEL]\n"
+        + "Example: " + COMMAND_WORD
+        + "v/ 78.9.";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Bloodsugar command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from blood sugar");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.BloodSugarCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -32,6 +33,12 @@ import seedu.address.testutil.PersonUtil;
 public class AddressBookParserTest {
 
     private final AddressBookParser parser = new AddressBookParser();
+
+    @Test
+    public void parseCommand_bloodSugar() throws Exception {
+        assertTrue(parser.parseCommand(BloodSugarCommand.COMMAND_WORD) instanceof BloodSugarCommand);
+    }
+
 
     @Test
     public void parseCommand_add() throws Exception {
